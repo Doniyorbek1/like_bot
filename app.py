3e427ff6a4a3007076e10c6126bf91b54c30c1de
Doniyorbek <programmmerqety1@gmail.com>
2023-02-24 10:14:19 +0500

    webhook

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask,request
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext,CallbackQueryHandler, Dispatcher
+from telegram import Update,  Bot
+from handler import start, get_image, callback_like
+
+import os
+
+# get token from environment variable
+TOKEN = os.environ['TOKEN'] 
+
+bot = Bot(TOKEN)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
+    
+    data = request.get_json(force=True)
+    
+    #dispatcher
+    dp : Dispatcher = Dispatcher(bot, None, workers=0)
+    
+    #update
+    update: Update = Update.de_json(data, bot)
+    
     print(request.form)
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.photo, get_image))
+    dp.add_handler(CallbackQueryHandler(callback_like))
+    
+    dp.process_update(update)
     return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
